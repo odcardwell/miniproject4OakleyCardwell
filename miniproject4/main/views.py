b4fe9d52a8fdbc7068bb96d2039b83f3d26c7999
Oakley Cardwell <odcardwell@mail.fhsu.edu>
2024-10-31 20:58:25 -0400

    Added ContactMessage model, admin setup, and contact form

--- miniproject4/main/views.py
+++ miniproject4/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'main/home.html')
@@ -9,7 +9,15 @@ def about(request):
     return render(request, 'main/about.html')
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'main/contact.html', {'form': form})
 
 def services(request):
     return render(request, 'main/services.html')
