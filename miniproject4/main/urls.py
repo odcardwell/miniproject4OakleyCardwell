6ab3c84951bf069892c7d01c99eb4268a71bc8e4
Oakley Cardwell <odcardwell@mail.fhsu.edu>
2024-10-31 21:43:45 -0400

    Implemented user registration, fixed user logout bug

--- miniproject4/main/urls.py
+++ miniproject4/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('services/', views.services, name='services'),
     path('portfolio/', views.portfolio, name='portfolio'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
+
--- miniproject4/main/views.py
+++ miniproject4/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, 'main/home.html')
@@ -19,9 +22,28 @@ def contact(request):
         form = ContactForm()
     return render(request, 'main/contact.html', {'form': form})
 
+@login_required
 def services(request):
     return render(request, 'main/services.html')
 
 def portfolio(request):
     return render(request, 'main/portfolio.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'main/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
 
